aa8d9911cbf38708bce6df031a4b1077b86d8487
Mabel Christina <maybelchristina@gmail.com>
2021-09-30 20:39:45 +0530

    [Mabel] Add: UC1 added

--- GenericUnitTest/MaxInFirstPosUnitTest.cs
+++ GenericUnitTest/MaxInFirstPosUnitTest.cs
@@ -13,5 +13,21 @@ namespace GenericUnitTest
             var actual = maxOfNumber.Max(a, b, c);
             Assert.AreEqual(a,actual);
         }
+        [Test]
+        public void GivenMaxInSecondPositin_WhenMaxNumberCheck_ThenReturnMax()
+        {
+            int a = 10, b = 22, c = 5;
+            MaxOfNumber maxOfNumber = new MaxOfNumber();
+            var actual = maxOfNumber.Max(a, b, c);
+            Assert.AreEqual(b, actual);
+        }
+        [Test]
+        public void GivenMaxInThirdPositin_WhenMaxNumberCheck_ThenReturnMax()
+        {
+            int a = 10, b = 2, c = 55;
+            MaxOfNumber maxOfNumber = new MaxOfNumber();
+            var actual = maxOfNumber.Max(a, b, c);
+            Assert.AreEqual(c, actual);
+        }
     }
 }
